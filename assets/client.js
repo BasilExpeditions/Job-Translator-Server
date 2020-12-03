974b364fe528b02b05d78fb62e5b692fe3da8ba4
Bastien Durand <basilexpeditions@gmail.com>
2020-12-03 22:53:42 +1100

    Final push :O

--- assets/client.js
+++ assets/client.js
@@ -5,13 +5,16 @@ $( document ).ready(function() {
     const dummyData = "Prestige WorldWide has an urgent need to hire a talented lead developer who shows an energetic spirit and can demonstrate their knowledge using cutting edge technology such as C#, React, Node, Assembly & has a strong foundation in Test Driven Development. The right candidate will be a team player who loves working in a fast paced environment with minimal supervision. Our team is passionate and is currently using agile as a day to day process. This job entails a dynamic environment with plenty of great benefits and perks. A bonus to have is web design & 1000 years experience coding."
 
     $("#translateButton").click( async (event) => {
+      $('#results ul').empty()
       const jobDescription = $('#inputField').val().toLowerCase()
-      // Scroll animation on translate button
+      // Scroll animation onClick translate button
       $("html, body").animate({
         scrollTop: $('html, body').get(0).scrollHeight
       }, 2500);
 
-      // For this to work locally, change the below url to 'localhost:1337/results'
+
+      // For this to work locally, change the below url to 'http://localhost:1337/results'
+      // https://job-translator.herokuapp.com/results
 
       await $.ajax({url: "https://job-translator.herokuapp.com/results", success: function(result) {
 
@@ -23,13 +26,14 @@ $( document ).ready(function() {
           counter++
           if(jobDescription.includes(x)) {
 
-            $('#results ul').append(`<li style='text-decoration: none; padding-top: 1em;'><strong>${x}</strong> &rarr; ${values[index]}</li>`)
+            $('#results ul').append(`<li class="liText" style='text-decoration: none; padding-top: 1em;'><strong>${x}</strong> &rarr; ${values[index]}</li>`)
           }
         }
       )
 
       if (counter == keys.length) {
         return $('#results ul').append("<strong class='text-primary'>No more matches</strong>")
+
       }
 
       }})
@@ -46,7 +50,7 @@ $( document ).ready(function() {
       await $.ajax({url: `https://api.adzuna.com/v1/api/jobs/au/search/1?app_id=6969f274&app_key=985462037ccb638ed824f8b40b5b4986&results_per_page=20&what=software%20engineer`,
         contentType: "application/json",
          success: function(result) {
-           console.log(result)
+           // console.log(result)
 
            let stringToFormat = result.results[0].description
            var randomJobResult = result.results[Math.floor(Math.random() * result.results.length)];
@@ -74,6 +78,6 @@ $( document ).ready(function() {
     "ప్రభుత్వం ప్రజల హక్కులను ఉల్లంఘించినప్పుడు, తిరుగుబాటు అనేది ప్రజలకు మరియు ప్రజల యొక్క ప్రతి భాగానికి, హక్కుల యొక్క అత్యంత పవిత్రమైనది మరియు విధులకు అత్యంత అనివార్యమైనది." +
     "സർക്കാർ ജനങ്ങളുടെ അവകാശങ്ങൾ ലംഘിക്കുമ്പോൾ, കലാപം ജനങ്ങൾക്കും ജനങ്ങളുടെ ഓരോ ഭാഗത്തിനും അവകാശങ്ങളുടെ ഏറ്റവും പവിത്രവും കടമകളിൽ ഒഴിച്ചുകൂടാനാവാത്തതുമാണ്.")
     console.log('The first name is a route, attempt your answer with:')
-    console.log('https://job-translator.herokuapp.com/ANSWER_HERE')
+    console.log('https://job-translator.herokuapp.com/?????')
 
 });
