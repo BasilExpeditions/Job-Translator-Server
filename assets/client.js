f9904f63090224ab1f6bc8cd05a27df8c44d7b1e
Bastien Durand <basilexpeditions@gmail.com>
2020-12-03 16:12:26 +1100

    lots of style tweaks, responsiveness tweaks + code cleanup

--- assets/client.js
+++ assets/client.js
@@ -6,8 +6,14 @@ $( document ).ready(function() {
 
     $("#translateButton").click( async (event) => {
       const jobDescription = $('#inputField').val().toLowerCase()
+      // Scroll animation on translate button
+      $("html, body").animate({
+        scrollTop: $('html, body').get(0).scrollHeight
+      }, 2000);
 
-      await $.ajax({url: 'https://job-translator.herokuapp.com/results' || "http://localhost:1337/results", success: function(result) {
+      // 'https://job-translator.herokuapp.com/results' to be inserted instead of local
+
+      await $.ajax({url: "http://localhost:1337/results", success: function(result) {
 
       const values = Object.values(result)
 
@@ -17,27 +23,41 @@ $( document ).ready(function() {
           counter++
           if(jobDescription.includes(x)) {
 
-            $('#results ul').append(`<li style='text-decoration: none;'><strong>${x}</strong> &rarr; ${values[index]}</li>`)
+            $('#results ul').append(`<li style='text-decoration: none; padding-top: 1em;'><strong>${x}</strong> &rarr; ${values[index]}</li>`)
           }
         }
       )
+
       if (counter == keys.length) {
-        return $('#results ul').append("<strong style='color:teal;'>No more matches</strong>")
+        return $('#results ul').append("<strong class='text-primary'>No more matches</strong>")
       }
+
       }})
     });
 
+    // Dummy data variable to showcase matching logic
     $('#getDummyDataButton').click( (event) => {
       $('#inputField').val(dummyData)
     })
 
+    // Random Job data using Adzuna API (Has weird formatting)
+    // Currently formatted for australia wide only
     $('#getRandomJobButton').click( async (event) => {
       await $.ajax({url: `https://api.adzuna.com/v1/api/jobs/au/search/1?app_id=6969f274&app_key=985462037ccb638ed824f8b40b5b4986&results_per_page=20&what=software%20engineer`,
         contentType: "application/json",
          success: function(result) {
+           console.log(result)
+
            let stringToFormat = result.results[0].description
            var randomJobResult = result.results[Math.floor(Math.random() * result.results.length)];
+
+           // Format came back with html tags within the data
+           // console.log(randomJobResult)
+
            let regex = /(<([^>]+)>)/ig
+           // REGEX i = Insensitive flag = Case-Insensitive matching
+           // REGEX g = Global flag = find all matches rather then stopping at first
+
            let formatted = randomJobResult.description.replaceAll(regex, "")
            $('#inputField').val(formatted)
       }})
---
+++ translationValues.js
@@ -0,0 +1,22 @@
+const translationValues = {
+  "software engineer": "be a damn good stack overflow surfer",
+  "software developer": "be a damn good stack overflow surfer",
+  "junior developer": "theyre looking for a basic googler",
+  "senior developer": "theyre looking for an expert googler",
+  "lead developer": "theyre looking for a lead googler to lead a team of googlers",
+  "energetic": "don't stop till you drop",
+  "cutting edge technology": "do what everyone else is doing",
+  "fast paced environment": "your job will be constant firefighting",
+  "team player": "must not question authority",
+  "minimal supervision": "you’ll be the one we blame when something goes wrong",
+  "agile": "we have daily standups",
+  "market leader": "recently started making a profit",
+  "rockstar developer": "you will work very long hours with impossible deadlines",
+  "urgent need": "our other rockstar just left and no one understands the code",
+  "dynamic environment": "our leadership keeps changing priorities",
+  "self starter": "we have no process",
+  "passionate": "perseveres through regular death marches",
+  "web design": "how good are you at downloading themes"
+}
+
+exports.translationValues = translationValues;
